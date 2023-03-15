cfd0ee039989154dd9c37cec60b8408dd1be5f2e
Clement A. Quaye <clemaddo@gmail.com>
2023-03-15 02:36:45 +0000

    Initial commit: python programming with turtle module

---
+++ turtle/intro2turtle.py
@@ -0,0 +1,28 @@
+import turtle
+rob = turtle.Turtle()
+
+#Testing out multiple lists
+rob.color("blue")
+for point in [-1,0,-1,0]:
+    rob.forward(80)
+    rob.right(135)
+
+
+
+#Rob drawing a 8-point star
+""""
+rob.color("black")
+
+for point in range(8):
+    rob.forward(80)
+    rob.right(135)
+
+#Rob drawing a hectagon
+
+rob.color("red")
+for point in range(6):
+    rob.forward(80)
+    rob.right(60)
+"""
+
+
