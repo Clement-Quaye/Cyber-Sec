83fdee2456e7182c92930f09a6d87f7e827de7a7
Leslie1302 <102149924+Leslie1302@users.noreply.github.com>
2023-03-15 15:06:21 +0000

    Update intro2turtle.py
    added the mainloop function to keep the window open after the script is done running and you put quotation marks on the part of the program meant to draw the star and the hectagon so I deleted the quotes.

--- turtle/intro2turtle.py
+++ turtle/intro2turtle.py
@@ -10,7 +10,6 @@ for point in [-1,0,-1,0]:
 
 
 #Rob drawing a 8-point star
-""""
 rob.color("black")
 
 for point in range(8):
@@ -23,6 +22,7 @@ rob.color("red")
 for point in range(6):
     rob.forward(80)
     rob.right(60)
-"""
+    
+turtle.mainloop()    
 
 
